ea0be1479acb353c3dc359aa39453423ff4a01db
yinpengfwi <935580670@qq.com>
2025-03-10 16:03:46 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://xxx.js
+^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/yinpengfwi/-/refs/heads/main/1.js
 
 [mitm] 
 hostname = api.dragonlongzhu.cn
